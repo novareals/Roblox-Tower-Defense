b8189ab55d9eea610780911b2a482def034907e5
🐔 Shedletsky 🍗 <kevinfrumau@gmail.com>
2025-05-13 11:03:40 +0700

    Update TowerShop.lua

--- TowerShop.lua
+++ TowerShop.lua
@@ -5,31 +5,36 @@ local TowerShop = {
 		["ImageAsset"] = "rbxassetid://76261305353446",
 		["Price"] = 0,
 	},
-	
+
 	["Elliot"] = {
 		["Name"] = "Elliot",
 		["ImageAsset"] = "rbxassetid://76261305353446",
 		["Price"] = 50,
 	},
-	
+
 	["007n7"] = {
 		["Name"] = "007n7",
 		["ImageAsset"] = "rbxassetid://76261305353446",
 		["Price"] = 100,
 	},
-	
+
 	["Guest 1337"] = {
 		["Name"] = "Guest 1337",
 		["ImageAsset"] = "rbxassetid://76261305353446",
 		["Price"] = 100,
 	},
-	
+
 	["Dusekkar"] = {
 		["Name"] = "Dusekkar",
 		["ImageAsset"] = "rbxassetid://76261305353446",
 		["Price"] = 500,
 	},
-	
+
+	["Shedletsky"] = {
+		["Name"] = "Shedletsky",
+		["ImageAsset"] = "rbxassetid://76261305353446", -- 🔄 Replace this with Shedletsky's real image asset ID if you have one
+		["Price"] = 1000,
+	},
 }
 
 return TowerShop
